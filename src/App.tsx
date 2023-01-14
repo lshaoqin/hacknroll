1d11066e024c68a5ff6ac180692a635df71e531f
SQ <shaoqin.lu@gmail.com>
2023-01-14 18:01:03 +0800

    resolve conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -10,49 +10,8 @@ function App() {
   );
 }
 
-function Image() {
-    
-}
-
-function Form() {
-    const [text, setText] = useState("");
-    return (
-        <>
-            <form onSubmit={(e) => {
-                    e.preventDefault();
-                    handleSubmit;
-                    }}>
-                <textarea
-                    text ={text}
-                    onChange={e => setText(e.target.value)}
-                    placeholder = "Insert text here"
-                />
-
-                <button 
-                    type = "submit"
-                    disabled={text.length === 0} 
-                />
-                    Submit answer
-                </button>
-            
-
-            </form>
-        </>
-
-    );
-  }
   
-function handleSubmit() {
-
-}
 
 
-function handleLeaderBoard(){
-    const [leaderboard, setLeaderboard] = useState([]);
-    setLeaderboard({...leaderboard, 
-
-    }) 
-
-}
 
 
